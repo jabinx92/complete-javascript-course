a32e906c4234b0e480da0151afe14aeae8c840a7
Jonathan Won <jabinx92@gmail.com>
2020-01-22 13:38:12 -0800

    jabin

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -15,34 +15,113 @@ structuring our code with MODULES
 
 //data encapsulation - hide from global scope certain information with IIFE
 //api - application program interface
+var budgetController = (function () {
 
-//BUDGET CONTROLLER
-var budgetController = (function() {
-   // some code
+
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, value) {
+            var newItem, ID;
+
+            //[1,2,3,4,5], next ID = 6
+            //[1,2,4,6,8], next ID = 9
+
+            // ID = last ID + 1
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+
+            //Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, value);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, value);
+            }
+
+            //Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            //Return the new element
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    };
+
 })();
 
 
+
 //UI CONTROLLER
+var UIController = (function () {
 
-
-
-var UIController = (function() {
-    
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
-    }
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expense__list'
+    };
 
     return {
         getInput: function () {
-
             return {
-                type : document.querySelector(DOMstrings.inputType).value, //Will be either inc or exp
-                description : document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
-            };
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        addListItem: function (obj, type) {
+            var html, newHtml, element;
+            //create html string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            //replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value', obj.value);
+
+            //insert the html into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+
+
         },
 
         getDOMstrings: function () {
@@ -52,38 +131,48 @@ var UIController = (function() {
 
 })();
 
-// GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl, UICtrl){
+//GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMstrings();
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function () {
-        // 1. get the field input data
-        var input = UICtrl.getInput();
-        console.log(input);
+        var input, newItem;
+        // 1. get the filled input data
+        input = UICtrl.getInput();
 
         // 2. add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        // 3. add the new item to the user interace
+        // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
-        // 4. calculate the budget
+        // 4. Calculate the budget
 
-        // 5. display the budget on the UI
+        // 5. Display the budget on the UI
 
+    };
+
+    return {
+        init: function () {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
-        }
-
-    })
 
 })(budgetController, UIController);
 
 
-
-
+controller.init();
