3246ad7b456fd54932c2fc4184b3a7d67d6f3530
Jonathan Won <jabinx92@gmail.com>
2020-01-31 14:15:34 -0800

    hello

--- 6-budgety/starter/app1.js
+++ 6-budgety/starter/app1.js
@@ -1,6 +1,65 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
     
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+
+            //[1 2 3 4 5], next ID = 6
+
+            //[1 2 4 6 8], next ID = 9
+
+            // ID = last ID + 1
+
+            //create new id
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            } 
+            
+            //create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // return the new element
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    };
+
 })();
 
 //UI CONTROLLER
@@ -50,11 +109,13 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 
     var ctrlAddItem = function () {
+        var input, newItem;
 
         // 1. Get the field input data
-        var input = UICtrl.getinput();
+        input = UICtrl.getinput();
 
         // 2. add item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. add the item to the UI
 
